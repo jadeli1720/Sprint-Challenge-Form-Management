b47721dfe4948e7b5d7dff01ccbd1893ff34bec0
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-09 09:49:46 -0600

    Created Client react app, added testing, formik; and yup dependencies. Deleted unneccessary files, and created first test

---
+++ client/src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+
+import App from './App';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it("renders without crashing", () => {
+    render(<App />);
+  });
+
+  it('renders App Title', () => {
+    const title = render(<App/>);
+    title.getByText(/Registration/i);
+  })
+});
